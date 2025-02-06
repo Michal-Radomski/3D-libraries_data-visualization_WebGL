ec2c6d28c34aa8caaffaea1d1c37477a2df77727
Michal Radomski <michalra@op.pl>
2025-02-06 02:04:15 +0100

    React Three Fiber course - commit 04

--- 01_React-Three-Fiber/01/src/App.tsx
+++ 01_React-Three-Fiber/01/src/App.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 
 import "./App.scss";
-import Example from "./Example";
+// import Example from "./Example";
 
 const App = (): React.JSX.Element => {
   return (
     <React.Fragment>
-      <Example />
-      {/* <mesh>
-      <boxGeometry />
-      <meshBasicMaterial color={0x00ff00} wireframe />
-    </mesh> */}
+      {/* <Example /> */}
+      <mesh>
+        <boxGeometry />
+        <meshBasicMaterial
+          // color={"#00ff00"}
+          color={0x00ff00}
+          wireframe={true}
+        />
+      </mesh>
     </React.Fragment>
   );
 };

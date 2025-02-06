8aab6b30cfa60e29b3be8eb08a4dc85102e651c1
Michal Radomski <michalra@op.pl>
2025-02-06 01:29:08 +0100

    React Three Fiber course - commit 03

--- 01_React-Three-Fiber/01/src/Example.tsx
+++ 01_React-Three-Fiber/01/src/Example.tsx
@@ -1,40 +1,42 @@
 import React from "react";
 import * as THREE from "three";
 
-const Example = (): React.JSX.Element => {
+const Example: React.FC = () => {
   const width: number = window.innerWidth,
     height: number = window.innerHeight;
 
-  //* Init
-  const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-  camera.position.z = 1;
+  React.useEffect(() => {
+    //* Init
+    const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+    camera.position.z = 1;
 
-  const scene: THREE.Scene = new THREE.Scene();
+    const scene: THREE.Scene = new THREE.Scene();
 
-  const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  const material: THREE.MeshNormalMaterial = new THREE.MeshNormalMaterial();
+    const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    const material: THREE.MeshNormalMaterial = new THREE.MeshNormalMaterial();
 
-  const mesh: THREE.Mesh<THREE.BoxGeometry, THREE.MeshNormalMaterial, THREE.Object3DEventMap> = new THREE.Mesh(
-    geometry,
-    material
-  );
-  scene.add(mesh);
+    const mesh: THREE.Mesh<THREE.BoxGeometry, THREE.MeshNormalMaterial, THREE.Object3DEventMap> = new THREE.Mesh(
+      geometry,
+      material
+    );
 
-  const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(width, height);
-  renderer.setAnimationLoop(animate);
-  document.body.appendChild(renderer.domElement);
+    scene.add(mesh);
 
-  //* Animation
-  function animate(time: number) {
-    mesh.rotation.x = time / 2000;
-    mesh.rotation.y = time / 1000;
+    const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(width, height);
+    renderer.setAnimationLoop(animate);
+    document.body.appendChild(renderer.domElement);
 
-    renderer.render(scene, camera);
-  }
+    //* Animation
+    function animate(time: number) {
+      mesh.rotation.x = time / 2000;
+      mesh.rotation.y = time / 1000;
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return <React.Fragment>{null as any}</React.Fragment>;
+      renderer.render(scene, camera);
+    }
+  }, [height, width]);
+
+  return <React.Fragment>{null}</React.Fragment>;
 };
 
 export default Example;

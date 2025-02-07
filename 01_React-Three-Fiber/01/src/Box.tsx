0236a45449e78c7d876fcf028831f05765ea4aeb
Michal Radomski <michalra@op.pl>
2025-02-07 23:08:37 +0100

    React Three Fiber course - commit 10

--- 01_React-Three-Fiber/01/src/Box.tsx
+++ 01_React-Three-Fiber/01/src/Box.tsx
@@ -14,6 +14,9 @@ const Box: React.FC<Props> = (props: Props): React.JSX.Element => {
   const meshRef = React.useRef<Mesh<BufferGeometry<NormalBufferAttributes>, Material | Material[], Object3DEventMap>>(null);
   const materialRef = React.useRef<MeshBasicMaterial>(null);
 
+  const [hovered, setHover] = React.useState<boolean>(false);
+  const [rotate, setRotate] = React.useState<boolean>(false);
+
   React.useEffect(() => {
     if (meshRef?.current && materialRef?.current) {
       // console.log("meshRef:", meshRef);
@@ -25,9 +28,11 @@ const Box: React.FC<Props> = (props: Props): React.JSX.Element => {
   useFrame((_state: RootState, delta: number) => {
     // console.log("_state", _state);
     // console.log("delta:", delta);
-    meshRef.current!.rotation.x += 1 * delta;
-    meshRef.current!.rotation.y += 0.5 * delta;
-    meshRef.current!.rotation.y = Math.sin(_state?.clock?.getElapsedTime());
+    if (rotate) {
+      meshRef.current!.rotation.x += 1 * delta;
+      meshRef.current!.rotation.y += 0.5 * delta;
+      meshRef.current!.rotation.y = Math.sin(_state?.clock?.getElapsedTime());
+    }
   });
 
   return (
@@ -35,19 +40,23 @@ const Box: React.FC<Props> = (props: Props): React.JSX.Element => {
       <mesh
         {...props}
         ref={meshRef}
-        onPointerDown={(event: ThreeEvent<PointerEvent>) => console.log("pointer down " + event.object.name)}
-        onPointerUp={(event: ThreeEvent<PointerEvent>) => console.log("pointer up " + event.object.name)}
-        onPointerOver={(event: ThreeEvent<PointerEvent>) => console.log("pointer over " + event.object.name)}
-        onPointerOut={(event: ThreeEvent<PointerEvent>) => console.log("pointer out " + event.object.name)}
-        onUpdate={(self: Mesh<BufferGeometry<NormalBufferAttributes>, Material | Material[], Object3DEventMap>) =>
-          console.log("self:", self)
-        }
-        onWheel={() => console.log("wheel spins")}
+        scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+        // onPointerDown={(event: ThreeEvent<PointerEvent>) => console.log("pointer down " + event.object.name)}
+        // onPointerUp={(event: ThreeEvent<PointerEvent>) => console.log("pointer up " + event.object.name)}
+        // onPointerOver={(event: ThreeEvent<PointerEvent>) => console.log("pointer over " + event.object.name)}
+        // onPointerOut={(event: ThreeEvent<PointerEvent>) => console.log("pointer out " + event.object.name)}
+        // onUpdate={(self: Mesh<BufferGeometry<NormalBufferAttributes>, Material | Material[], Object3DEventMap>) =>
+        //   console.log("self:", self)
+        // }
+        // onWheel={() => console.log("wheel spins")}
         onClick={() => console.log("click")}
         onDoubleClick={(event: ThreeEvent<MouseEvent>) => console.log("event:", event)}
+        onPointerDown={() => setRotate(!rotate)}
+        onPointerOver={() => setHover(true)}
+        onPointerOut={() => setHover(false)}
       >
         <boxGeometry />
-        <meshBasicMaterial color={0x00ff00} wireframe={props.wireframe} ref={materialRef} />
+        <meshBasicMaterial color={hovered ? 0xff0000 : 0x00ff00} wireframe={props.wireframe} ref={materialRef} />
       </mesh>
     </React.Fragment>
   );

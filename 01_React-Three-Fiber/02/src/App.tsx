ca42093b9a067dad6b7a232f787b808e0c4992d3
Michal Radomski <michalra@op.pl>
2025-02-09 00:52:03 +0100

    React Three Fiber course - commit 26

--- 01_React-Three-Fiber/02/src/App.tsx
+++ 01_React-Three-Fiber/02/src/App.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { Canvas, ObjectMap, useLoader } from "@react-three/fiber";
-import { Circle, OrbitControls, Stats } from "@react-three/drei";
+import { Environment, OrbitControls, Sphere, Stats } from "@react-three/drei";
 
 import "./App.scss";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { GLTF, GLTFLoader } from "three/examples/jsm/Addons.js";
 
 const App = (): React.JSX.Element => {
   const gltf: GLTF & ObjectMap = useLoader(GLTFLoader, "./monkey.glb");
+  // console.log("gltf:", gltf);
 
   return (
     <React.Fragment>
-      <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
-        <directionalLight position={[3.3, 1.0, 4.4]} castShadow intensity={Math.PI * 2} />
-        <primitive object={gltf.scene} position={[0, 1, 0]} children-0-castShadow />
-        <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
+      <Canvas camera={{ position: [-0.5, 1, 2] }} shadows={true}>
+        <Environment
+          files="/forest_slope_1k.hdr"
+          // preset="forest"
+          background={true}
+          backgroundBlurriness={0.0}
+        />
+        <Sphere args={[0.25]} />
+        <directionalLight position={[3.3, 1.0, 4.4]} castShadow={true} intensity={Math.PI * 2} />
+        <primitive object={gltf.scene} position={[0, 1, 0]} children-0-castShadow={true} />
+        {/* <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow={true}>
           <meshStandardMaterial />
-        </Circle>
-        <OrbitControls target={[0, 1, 0]} />
+        </Circle> */}
+        <OrbitControls target={[0, 1, 0]} autoRotate={true} />
         <axesHelper args={[5]} />
         <Stats />
       </Canvas>

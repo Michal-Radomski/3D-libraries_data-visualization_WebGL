4884f2c85b70ba690ffb3026bfb709d5fdf123b2
Michal Radomski <michalra@op.pl>
2025-02-17 23:43:01 +0100

    ThreeJS and Shaders course - commit 0

--- 03_ThreeJS_&_Shaders/01/src/main.ts
+++ 03_ThreeJS_&_Shaders/01/src/main.ts
@@ -7,13 +7,13 @@ import "./style.scss";
 //* Scene Mesh Camera Renderer
 //* 1. Scene
 const scene: THREE.Scene = new THREE.Scene();
-scene.fog = new THREE.Fog(0xcccccc, 10, 15);
+scene.fog = new THREE.Fog(0xcccccc, 1, 200);
 
 //* 2. Mesh (Objects)
 const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 1, 2, 2); //* Cube
 // const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: "purple" });
 // const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: "#A020F0" });
-const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0xa020f0, fog: true, wireframe: false });
+const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0xa020f0, fog: true, wireframe: true });
 const mesh: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial, THREE.Object3DEventMap> = new THREE.Mesh(
   geometry,
   material
@@ -25,7 +25,9 @@ const aspect = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height, 1, 2000); // near value is 1, and far value is 2000
+
+// https://en.wikipedia.org/wiki/Viewing_frustum
+const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height, 1, 2000); // Near: 1, and Far: 2000
 camera.position.z = 3;
 camera.position.x = 1;
 camera.position.y = 1;
@@ -33,19 +35,20 @@ camera.zoom = 1;
 scene.add(camera);
 
 //* 4. Renderer
-const canvas = document.querySelector("canvas.draw") as HTMLCanvasElement; //select the canvas element
-const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ canvas }); //add the WebGLRenderer
-renderer.setSize(aspect.width, aspect.height); //Renderer size
-renderer.render(scene, camera); //display what the camera in the scene captured
+const canvas = document.querySelector("canvas.draw") as HTMLCanvasElement; // Select the canvas element
+const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ canvas, precision: "highp", alpha: false }); // Add the WebGLRenderer
+renderer.setSize(aspect.width, aspect.height); // Renderer size
+renderer.render(scene, camera); // Display what the camera in the scene captured
 
 // Orbit Controls
 const controls: OrbitControls = new OrbitControls(camera, canvas as HTMLCanvasElement);
+// console.log("controls:", controls);
 
 // Animation loop
-function animate(): void {
+(function animate(): void {
   requestAnimationFrame(animate);
   controls.update(); // IMPORTANT: Update the controls in the animation loop
+  controls.autoRotate = true;
+  controls.enablePan = true;
   renderer.render(scene, camera);
-}
-
-animate();
+})();

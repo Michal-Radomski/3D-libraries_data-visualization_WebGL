091bcfab0d3f6c4cba53527ae73fc13be241a677
Michal Radomski <michalra@op.pl>
2025-02-26 00:41:43 +0100

    ThreeJS and Shaders course - commit 45

--- 03_ThreeJS_&_Shaders/03/src/main.ts
+++ 03_ThreeJS_&_Shaders/03/src/main.ts
@@ -1,1 +1,69 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+
 import "./style.scss";
+
+// Scene
+const scene: THREE.Scene = new THREE.Scene();
+
+// Mesh
+const geometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(1, 1);
+const material: THREE.MeshStandardMaterial = new THREE.MeshStandardMaterial();
+// const material: THREE.RawShaderMaterial = new THREE.RawShaderMaterial({
+//   // vertexShader: vShader,
+//   // fragmentShader: fShader,
+// });
+const mesh: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshStandardMaterial, THREE.Object3DEventMap> = new THREE.Mesh(
+  geometry,
+  material
+);
+scene.add(mesh);
+
+// Camera
+const aspect = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
+camera.position.z = 2;
+scene.add(camera);
+
+//* Renderer
+const canvas = document.querySelector("canvas.draw") as HTMLCanvasElement;
+const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({ canvas, alpha: true, antialias: true });
+renderer.setSize(aspect.width, aspect.height);
+
+//* OrbitControls
+const orbitControls: OrbitControls = new OrbitControls(camera, canvas);
+orbitControls.enableDamping = true;
+orbitControls.enableZoom = true;
+orbitControls.enableRotate = true;
+orbitControls.autoRotate = true;
+orbitControls.autoRotateSpeed = 0.2;
+
+// const clock = new THREE.Clock();
+
+//* Animate
+(function animate(): void {
+  // const elapsedTime: number = clock.getElapsedTime();
+
+  orbitControls.update(); // IMPORTANT: Update the controls in the animation loop
+
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animate);
+})();
+
+//* Resizing
+window.addEventListener("resize", (): void => {
+  // New size
+  aspect.width = window.innerWidth;
+  aspect.height = window.innerHeight;
+
+  // New AspectRatio
+  camera.aspect = aspect.width / aspect.height;
+  camera.updateProjectionMatrix();
+
+  // New RendererSize
+  renderer.setSize(aspect.width, aspect.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});

37eeca1beec9583758856cd17c14a370c4a9efeb
Michal Radomski <michalra@op.pl>
2025-03-04 00:50:26 +0100

    ThreeJS course - commit 23

---
+++ 04_ThreeJS_&_more/03_React_Three_Drei/src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+
+import "./App.scss";
+
+const App = (): React.JSX.Element => {
+  return (
+    <React.Fragment>
+      <Canvas
+        gl={{
+          antialias: true, //* default: true
+          alpha: true, //* default: true
+        }}
+        camera={{
+          fov: 75,
+          near: 0.1,
+          far: 100,
+          position: [2, 2, 5],
+        }}
+      >
+        <ambientLight intensity={0.1} />
+        <directionalLight position={[0, 0, 5]} color="red" />
+      </Canvas>
+    </React.Fragment>
+  );
+};
+
+export default App;

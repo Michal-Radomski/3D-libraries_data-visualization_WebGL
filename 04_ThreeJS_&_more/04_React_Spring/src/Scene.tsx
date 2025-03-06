24912b63cc1b7cecbe34082a157f972f8e743487
Michal Radomski <michalra@op.pl>
2025-03-06 23:06:31 +0100

    ThreeJS course - commit 36

---
+++ 04_ThreeJS_&_more/04_React_Spring/src/Scene.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { a, useSpring } from "@react-spring/three";
+
+const Scene = (): React.JSX.Element => {
+  const [click, setClick] = React.useState<boolean>(false);
+
+  const { scale, color } = useSpring({
+    from: { scale: click ? 1 : 2, color: click ? "orange" : "hotpink" },
+    scale: click ? 2 : 1,
+    color: click ? "hotpink" : "orange",
+  });
+  console.log("scale:", scale);
+
+  const clickHandler = (): void => {
+    setClick(!click);
+  };
+
+  return (
+    <React.Fragment>
+      <a.mesh onClick={clickHandler} scale={scale}>
+        <boxGeometry />
+        <a.meshBasicMaterial color={color} />
+      </a.mesh>
+    </React.Fragment>
+  );
+};
+
+export default Scene;

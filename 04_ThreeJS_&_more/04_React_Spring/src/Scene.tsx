03dc91fb685e42dac972930337fa19c0c82aeca3
Michal Radomski <michalra@op.pl>
2025-03-06 23:14:53 +0100

    ThreeJS course - commit 38

--- 04_ThreeJS_&_more/04_React_Spring/src/Scene.tsx
+++ 04_ThreeJS_&_more/04_React_Spring/src/Scene.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { a, useSpring } from "react-spring";
-// import { a, useSpring } from "@react-spring/three";
+// import { a, useSpring } from "@react-spring/three"; //* V1
+import { a, useSpring } from "react-spring"; //* V2
+import { BufferGeometry, Material, Mesh, NormalBufferAttributes, Object3DEventMap } from "three";
 
 const Scene = (): React.JSX.Element => {
   const [click, setClick] = React.useState<boolean>(false);
+
+  const meshRef = React.useRef<Mesh<BufferGeometry<NormalBufferAttributes>, Material, Object3DEventMap>>(null);
 
   const { scale, color } = useSpring({
     from: { scale: click ? 1 : 2, color: click ? "orange" : "hotpink" },
@@ -18,7 +21,7 @@ const Scene = (): React.JSX.Element => {
 
   return (
     <React.Fragment>
-      <a.mesh onClick={clickHandler} scale={scale}>
+      <a.mesh onClick={clickHandler} scale={scale} ref={meshRef}>
         <boxGeometry />
         <a.meshBasicMaterial color={color} />
       </a.mesh>

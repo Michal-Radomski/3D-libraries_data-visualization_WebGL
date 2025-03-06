7ac581850adfe1deb0e0caf67a0eadfaa77c1081
Michal Radomski <michalra@op.pl>
2025-03-07 00:53:35 +0100

    ThreeJS course - commit 44

--- 04_ThreeJS_&_more/04_React_Spring/src/Scene.tsx
+++ 04_ThreeJS_&_more/04_React_Spring/src/Scene.tsx
@@ -1,48 +1,104 @@
-//* V2
+//* V3
 import React from "react";
-import { animated, useSpring } from "@react-spring/three";
+import { animated, Lookup, SpringRef, useSpring, useSpringRef } from "@react-spring/three";
 
 const Scene = (): React.JSX.Element => {
-  const [clicked, setClicked]: [clicked: boolean, setClicked: React.Dispatch<React.SetStateAction<boolean>>] =
-    React.useState<boolean>(false);
+  // const [spring, api] = useSpring(() => ({
+  //   from: { x: -2 },
+  // }));
+  // console.log("api:", api);
 
-  let n: number = 0;
-  const { x, y, color } = useSpring({
-    from: { color: "hotpink", x: -2, y: 0 },
-    // to: { color: "yellow", x: 2, y: -1 },
-    to: [
-      { color: "yellow", x: 2 },
-      { color: "cyan", y: 2 },
-      { color: "greenyellow", x: -2 },
-      { color: "hotpink", y: -2 },
-    ],
-    loop: () => 3 > n++, //should return a boolean value
-    delay: 1000,
-    // Reverse: clicked,
-    pause: clicked,
-    // Reset: clicked,
-    config: { mass: 20, tension: 700, friction: 15, clamp: false },
-    onStart: () => console.log("onStart"),
-    onRest: () => console.log("onRest"),
-    onPause: () => console.log("onPause"),
-    onResume: () => console.log("onResume"),
+  const springRef: SpringRef<Lookup<unknown>> = useSpringRef();
+
+  const spring = useSpring({
+    ref: springRef,
+    from: { x: -2 },
   });
 
   const clickHandler = (): void => {
-    setClicked(!clicked);
+    springRef.start({
+      to: { x: 2 },
+      config: { duration: 5000 },
+    });
+  };
+
+  const pointerOverHandler = (): void => {
+    springRef.pause();
+  };
+
+  const pointerOutHandler = (): void => {
+    springRef.resume();
   };
 
   return (
     <React.Fragment>
-      <animated.mesh position-x={x} rotation-y={x} position-y={y} onClick={clickHandler}>
+      <animated.mesh
+        position-x={spring.x}
+        onClick={clickHandler}
+        onPointerOver={pointerOverHandler}
+        onPointerOut={pointerOutHandler}
+      >
         <boxGeometry />
-        <animated.meshBasicMaterial color={color} />
+        <meshBasicMaterial color="orange" />
       </animated.mesh>
     </React.Fragment>
   );
 };
 
 export default Scene;
+
+//* V2
+// import React from "react";
+// import { animated, useSpring } from "@react-spring/three";
+
+// const Scene = (): React.JSX.Element => {
+//   const [clicked, setClicked]: [clicked: boolean, setClicked: React.Dispatch<React.SetStateAction<boolean>>] =
+//     React.useState<boolean>(false);
+
+//   let n: number = 0;
+//   const { x, y, color } = useSpring({
+//     from: { color: "hotpink", x: -2, y: 0 },
+//     // to: { color: "yellow", x: 2, y: -1 },
+//     to: [
+//       { color: "yellow", x: 2 },
+//       { color: "cyan", y: 2 },
+//       { color: "greenyellow", x: -2 },
+//       { color: "hotpink", y: -2 },
+//     ],
+//     loop: () => 3 > n++, //should return a boolean value
+//     delay: 1000,
+//     // Reverse: clicked,
+//     pause: clicked,
+//     // Reset: clicked,
+//     config: {
+//       mass: 20,
+//       tension: 700,
+//       friction: 15,
+//       clamp: false,
+//       // duration: 500
+//       // velocity: 0,
+//     },
+//     onStart: () => console.log("onStart"),
+//     onRest: () => console.log("onRest"),
+//     onPause: () => console.log("onPause"),
+//     onResume: () => console.log("onResume"),
+//   });
+
+//   const clickHandler = (): void => {
+//     setClicked(!clicked);
+//   };
+
+//   return (
+//     <React.Fragment>
+//       <animated.mesh position-x={x} rotation-y={x} position-y={y} onClick={clickHandler}>
+//         <boxGeometry />
+//         <animated.meshBasicMaterial color={color} />
+//       </animated.mesh>
+//     </React.Fragment>
+//   );
+// };
+
+// export default Scene;
 
 //* V1
 // import React from "react";

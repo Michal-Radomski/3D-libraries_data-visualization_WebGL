914399291e446da8b5f93e014d4281540b96686e
Michal Radomski <michalra@op.pl>
2025-03-08 01:29:09 +0100

    ThreeJS course - commit 48

--- 04_ThreeJS_&_more/05_React_Three_Rapier/src/PhysicsScene.tsx
+++ 04_ThreeJS_&_more/05_React_Three_Rapier/src/PhysicsScene.tsx
@@ -10,25 +10,25 @@ const PhysicsScene = (): CustomElem => {
     <React.Fragment>
       <Physics gravity={[0, -9.81, 0]}>
         <RigidBody>
-          <mesh castShadow position={[0, 1.5, 0]}>
+          <mesh castShadow={true} position={[0, 1.5, 0]}>
             <boxGeometry />
             <meshStandardMaterial color="#CC3941" />
           </mesh>
-          <mesh castShadow position={[0, 1.5, 0]} scale={[0.25, 3, 1]} position-z={-2}>
+          {/* <mesh castShadow={true} position={[0, 1.5, 0]} scale={[0.25, 3, 1]} position-z={-2}>
+            <boxGeometry />
+            <meshStandardMaterial color="#CC3941" />
+          </mesh> */}
+        </RigidBody>
+
+        <RigidBody>
+          <mesh castShadow={true} position={[0, 1.5, 0]} scale={[0.25, 3, 1]} position-z={-2}>
             <boxGeometry />
             <meshStandardMaterial color="#CC3941" />
           </mesh>
         </RigidBody>
 
-        {/* <RigidBody>
-          <mesh castShadow position={[0, 1.5, 0]} scale={[0.25, 3, 1]} position-z={-2}>
-            <boxGeometry />
-            <meshStandardMaterial color="#CC3941" />
-          </mesh>
-        </RigidBody> */}
-
         <RigidBody type="fixed">
-          <mesh position-y={-1} rotation-x={-Math.PI * 0.5} receiveShadow>
+          <mesh position-y={-1} rotation-x={-Math.PI * 0.5} receiveShadow={true}>
             <boxGeometry args={[8, 8, 0.35]} />
             <meshStandardMaterial color="#C7CAC7" />
           </mesh>

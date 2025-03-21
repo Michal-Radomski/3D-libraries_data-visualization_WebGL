9e5ad36f9d11f5193e43f227478e29bd758e7c57
Michal Radomski <michalra@op.pl>
2025-03-21 21:23:33 +0100

    D3.js course - commit 06

--- 07_D3.js/01/src/main.ts
+++ 07_D3.js/01/src/main.ts
@@ -14,13 +14,13 @@ const marginBottom = 30;
 const marginLeft = 40;
 
 // Declare the x (horizontal position) scale.
-const x = d3
+const x: d3.ScaleTime<number, number, never> = d3
   .scaleUtc()
   .domain([new Date("2023-01-01"), new Date("2024-01-01")])
   .range([marginLeft, width - marginRight]);
 
 // Declare the y (vertical position) scale.
-const y = d3
+const y: d3.ScaleLinear<number, number, never> = d3
   .scaleLinear()
   .domain([0, 100])
   .range([height - marginBottom, marginTop]);
@@ -59,6 +59,28 @@ console.log("elem:", elem);
 
 const p: d3.Selection<d3.BaseType, unknown, HTMLElement, any> = d3.selectAll("p");
 console.log("p:", p);
+
+{
+  const data = [10, 20, 30, 40, 50];
+  const el: d3.Selection<d3.BaseType, number, d3.BaseType, unknown> = d3
+    .select("ul")
+    .selectAll("li")
+    .data(data)
+    .join(
+      (enter: d3.Selection<d3.EnterElement, number, d3.BaseType, unknown>) => {
+        return enter.append("li").style("color", "purple");
+      },
+      (update: d3.Selection<d3.BaseType, number, d3.BaseType, unknown>) => update.style("color", "green"),
+      (exit: d3.Selection<d3.BaseType, number, d3.BaseType, unknown>) => exit.remove()
+    )
+    .text((d: number) => d);
+
+  el.enter()
+    .append("li")
+    .text((d: number) => d);
+
+  el.exit().remove();
+}
 
 //* Data in D3.js
 (async function getData(): Promise<void> {

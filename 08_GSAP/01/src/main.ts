809c24b7aa41594118bf5e362163551953e122a9
Michal Radomski <michalra@op.pl>
2025-03-25 00:39:22 +0100

    GSAP course - commit 04

--- 08_GSAP/01/src/main.ts
+++ 08_GSAP/01/src/main.ts
@@ -6,5 +6,7 @@ import "./style.scss";
 const img1 = document.querySelector(".img1") as HTMLImageElement;
 const h1 = document.querySelector("h1") as HTMLHeadElement;
 
-gsap.to(img1, { x: 100, y: 100, duration: 2, backgroundColor: "red", rotation: 45 });
+// gsap.to(img1, { x: 100, y: 100, duration: 2, backgroundColor: "red", rotation: 45 });
 gsap.to(h1, { x: 100, y: 100, duration: 2, backgroundColor: "red", rotation: 45 });
+
+gsap.from(img1, { y: -300, opacity: 0, duration: 2, autoAlpha: 0 }); //* No flash effect!

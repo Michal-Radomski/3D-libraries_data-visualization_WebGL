64cc1b05f044f39fed4879a4a594404e9a562e1e
Michal Radomski <michalra@op.pl>
2025-03-25 01:03:46 +0100

    GSAP course - commit 07

--- 08_GSAP/01/src/main.ts
+++ 08_GSAP/01/src/main.ts
@@ -18,10 +18,28 @@ gsap.to(h1, { x: 100, y: 100, duration: 2, backgroundColor: "red", rotation: 45 
 // });
 
 gsap.from(img1, {
+  // gsap.from("img", {
   y: -300,
   duration: 2,
   autoAlpha: 0,
   rotation: 90,
   // ease: "power4.out",
   ease: "back.out(3.9)",
+  paused: false,
+  delay: 0.5,
+  repeat: 1, //* 1 move + 1 repeating
+  yoyo: false,
+  // stagger: {
+  //   each: 0.5,
+  // },
+  stagger: (index) => {
+    console.log({ index });
+    return index;
+  },
+});
+
+gsap.to(".btn", {
+  duration: 0.5,
+  stagger: 0.2,
+  x: "random(-100,100)",
 });

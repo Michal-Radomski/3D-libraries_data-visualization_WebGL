10355c9f32266da190559b30a2cf6688dc867abf
Michal Radomski <michalra@op.pl>
2025-03-26 01:04:49 +0100

    GSAP course - commit 16

--- 08_GSAP/02/src/main.ts
+++ 08_GSAP/02/src/main.ts
@@ -1,4 +1,23 @@
 import gsap from "gsap";
-console.log("gsap:", gsap);
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+// console.log("gsap:", gsap);
+// console.log("ScrollTrigger:", ScrollTrigger);
 
 import "./style.scss";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const header = document.querySelector("div.block.b2 h2") as HTMLHeadElement;
+// console.log("header:", header);
+
+gsap.from(header, {
+  duration: 1,
+  autoAlpha: 0,
+  y: 100,
+  scale: 0.5,
+  scrollTrigger: {
+    trigger: header,
+    markers: true,
+  },
+});

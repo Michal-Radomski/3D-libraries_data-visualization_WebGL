2a22ba62abf1fe3b890c4e9bccfe19e7a9507be2
Michal Radomski <michalra@op.pl>
2025-03-28 22:25:05 +0100

    Litte update

--- 09_Game_Canvas_JS/src/classes/Invader.ts
+++ 09_Game_Canvas_JS/src/classes/Invader.ts
@@ -5,10 +5,13 @@ import { InvaderProjectile } from "./InvaderProjectile";
 
 export class Invader {
   velocity: Position;
-  image!: HTMLImageElement;
+  // image!: HTMLImageElement;
+  private _image?: HTMLImageElement; // Use a private property with optional type
   width!: number;
   height!: number;
   position!: Position;
+  private loaded = false; // Flag to track if the image is loaded
+
   constructor({ position }: { position: Position }) {
     this.velocity = {
       x: 0,
@@ -19,29 +22,34 @@ export class Invader {
     image.src = invader;
     image.onload = (): void => {
       const scale = 1;
-      this.image = image;
+      // this.image = image;
+      this._image = image;
       this.width = image.width * scale;
       this.height = image.height * scale;
       this.position = {
         x: position.x,
         y: position.y,
       };
+      this.loaded = true; // Set loaded flag to true
     };
   }
 
   draw(): void {
     // c.fillStyle = 'red'
     // c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    if (this.image) {
-      c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+    if (this.loaded) {
+      if (this._image) {
+        c.drawImage(this._image, this.position.x, this.position.y, this.width, this.height);
+      }
     } else {
       console.log("Invader image not loaded");
     }
   }
-  update({ velocity }: { velocity: Position }): void {
-    if (!this.image) return;
 
-    if (this.image) {
+  update({ velocity }: { velocity: Position }): void {
+    if (!this._image) return;
+
+    if (this._image) {
       this.draw();
       this.position.x += velocity.x;
       this.position.y += velocity.y;
